cfe3fc39a0c61c9da0fb6a52840bd0badc44101c
Daniel Berthran <berthlearn@gmail.com>
2024-09-10 23:20:48 +0100

    Initialize login session

--- notable/__init__.py
+++ notable/__init__.py
@@ -18,7 +18,7 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 # Initialize the login manager
-# login_manager = LoginManager(app)
+login_manager = LoginManager(app)
 # login_manager.login_view = 'login'
 # login_manager.login_message_category = 'info'
 
