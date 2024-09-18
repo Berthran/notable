ba03a46503121a118b8607ce94392458b6c2cf8a
Daniel Berthran <berthlearn@gmail.com>
2024-09-18 01:16:44 +0100

    create FlaskForm objects for password reset

--- notable/forms.py
+++ notable/forms.py
@@ -75,3 +75,30 @@ class NoteForm(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     content = TextAreaField("Content") # A user can create an empty note
     save = SubmitField('Save')
+
+
+class RequestResetForm(FlaskForm):
+    '''
+    Handles password reset request
+    '''
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+     # Validatate the user account exists
+    def validate_email(self, email):
+        '''
+        Validates the user provided email
+        '''
+        # Check if the email already exists
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first')
+    
+
+class ResetPasswordForm(FlaskForm):
+    '''
+    Handles password reset
+    '''
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
