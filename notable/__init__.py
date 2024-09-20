e4e2915dbf1731899eb7e6af2ba4e15959672bc7
Daniel Berthran <berthlearn@gmail.com>
2024-09-20 11:26:34 +0100

    add configuration for mail server

--- notable/__init__.py
+++ notable/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
@@ -21,6 +23,17 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
 login_manager.login_message_category = 'info'
+
+# Constants for Mail Server
+app.config['MAIL_SERVER'] = "smtp.googlemail.com"
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+
+
+
 
 
 from notable import routes
